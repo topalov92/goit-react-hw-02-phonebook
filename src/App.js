c9c01cfdab0ab198f58797d05d52528818bf95b0
Volodymyr <lotoshead@gmail.com>
2021-12-27 23:58:37 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./Form/Form.js";
+import ContactList from "./ContactLIst/ContactList.js";
+import Filter from "./Filter/Filter.js";
+
+export default class App extends Component {
+  static defaultProps = {
+    contacts: [
+      { id: uuidv4(), name: "Rosie Simpson", number: "459-12-56" },
+      { id: uuidv4(), name: "Hermione Kline", number: "443-89-12" },
+      { id: uuidv4(), name: "Eden Clements", number: "645-17-79" },
+      { id: uuidv4(), name: "Annie Copeland", number: "227-91-26" },
+    ],
+  };
+
+  state = {
+    contacts: [...this.props.contacts],
+    filter: "",
+  };
+
+  addContact = (data) => {
+    this.setState({
+      contacts: [data, ...this.state.contacts],
+    });
+  };
+
+  changeFilter = (ev) => {
+    this.setState({
+      filter: ev.currentTarget.value,
+    });
+  };
+
+  visibleItems = () => {
+    const { contacts } = this.state;
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  deleteContact = (contactID) => {
+    // console.log(contactID)
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactID
+      ),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    const { changeFilter, addContact } = this;
+    return (
+      <>
+        <Form addContactItem={addContact} contacts={this.state.contacts} />
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList
+          itemsRender={this.visibleItems()}
+          deleteItem={this.deleteContact}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
